536ff304bf6b35efff8b8427b2136980a683764b
barbourshop <99221725+barbourshop@users.noreply.github.com>
2025-02-03 10:12:16 -0800

    Update playwright.config.ts
    changing to headless operation

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   reporter: 'list',
   use: {
     baseURL: 'http://localhost:5173',
-    headless: false,
+    headless: true,
     viewport: { width: 1280, height: 720 },
     trace: 'on',
     video: 'on-first-retry'
