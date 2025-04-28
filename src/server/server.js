022fe3fdffc8dd1e0d2b0aa416ef5fd83279d18d
Matt Barbour <mbarbo000@gmail.com>
2025-04-28 08:10:14 -0700

    adding back the dotenv stuff that got removed

--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+require('dotenv').config();
 
 // Import routes
 const customerRoutes = require('./routes/customerRoutes');
